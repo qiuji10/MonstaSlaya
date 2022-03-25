23abff1f83c39a84d55818a20150fb4647552c06
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-03-25 12:27:10 +0800

    Initial commit

---
+++ Assets/Scenes/1_Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Rigidbody2D rb;
+
+    Vector2 movement;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+    }
+}
