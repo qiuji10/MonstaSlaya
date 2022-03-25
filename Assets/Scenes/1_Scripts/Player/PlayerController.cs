30c6bf4e2f9fa172b07b039144098799dacb2ddb
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-03-25 12:47:01 +0800

    Switch Character , Cinemachine

--- Assets/Scenes/1_Scripts/Player/PlayerController.cs
+++ Assets/Scenes/1_Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
 
+    public GameObject C1, C2, C3;
     Rigidbody2D rb;
 
     Vector2 movement;
@@ -20,10 +21,34 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            SwitchCharacter();
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+    }
+
+    public void SwitchCharacter()
+    {
+        if (C1.activeInHierarchy)
+        {
+            C1.SetActive(false);
+            C2.SetActive(true);
+        }
+        else if (C2.activeInHierarchy)
+        {
+            C2.SetActive(false);
+            C3.SetActive(true);
+        }
+        else if (C3.activeInHierarchy)
+        {
+            C3.SetActive(false);
+            C1.SetActive(true);
+        }
     }
 }
